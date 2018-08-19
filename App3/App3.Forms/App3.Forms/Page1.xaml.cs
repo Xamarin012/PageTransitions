a2fd0fae288e0a3f1f9e609b68034be9a0bda957
Xamarin Patel <xamarin012@gmail.com>
2018-08-19 13:19:27 +0530

    Add project

---
+++ App3/App3.Forms/App3.Forms/Page1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        private void Page2Button_Clicked(object sender, EventArgs e)
+        {
+            var transitionNavigationPage = Parent as TransitionNavigationPage;
+
+            transitionNavigationPage.TransitionType = TransitionType.SlideFromLeft;
+            Navigation.PushAsync(new Page2());
+        }
+    }
+}
